ce3217dcb749f319c89152f6734898500c91f4f9
braryadwinder <yadwinderbrar92@gmail.com>
2017-07-21 18:54:21 -0400

    add

---
+++ bookstoreapp/Book.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookstoreapp
+{
+    public class Book
+    {
+        private String name;
+        private Author[] authors;
+        private double price;
+        private Date publication;
+        private int qty = 0;
+
+        public Book(string name, Author[] authors, double price)
+        {
+            this.name = name;
+            this.authors = authors;
+            this.price = price;
+        }
+
+        public Book(string name,Author[] authors, double price, int qty)
+        {
+            this.name = name;
+            this.authors = authors;
+            this.price = price;
+            this.qty = qty;
+        }
+
+        public String GetName()
+        {
+            return this.name;
+        }
+
+        public Author[] GetAuthors()
+        {
+            return this.authors;
+
+        }
+
+        public Date GetPublicationDate()
+        {
+            return this.Date;
+        }
+
+        public void SetPublicationDate(Date date)
+        {
+
+        }
+
+        public double GetPrice()
+        {
+            return this.price;
+        }
+
+        public void SetPrice(double price)
+        {
+            this.price = price;
+        }
+
+        public int GetQty()
+        {
+            return this.qty;
+        }
+
+        public void SetQty(int qty)
+        {
+            this.qty = qty;
+        }
+
+        public string toString()
+        {
+            return string.Format("");
+        }
+
+        public Author[] GetAuthorsNames()
+        {
+            return this.Author;
+        }
+    }
+}
---
+++ bookstoreapp/user.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookstoreapp
+{
+    class user
+    {
+
+        List<Author> favoriteAuthors { get; set; }
+        List<Book> ShoppingCart { get; set; }
+
+        public void AddAuthor(Author author)
+        {
+            favoriteAuthors.Add(author);
+        }
+        public Author[] GetAuthors()
+        {
+            return favoriteAuthors.ToArray();
+        }
+        //public Book[] GetShoppingCart()
+        //{
+        //    return ShoppingCart.ToArray();
+        //}
+        public void AddToCart(Book book)
+        {
+            ShoppingCart.Add(book);
+        }
+        public void RemoveAuthor(Author author)
+        {
+            favoriteAuthors.Remove(author);
+        }
+        public void RemoveFromCart(Book book)
+        {
+            ShoppingCart.Remove(book);
+        }
+        public string GetShoppingCart()
+        {
+            StringBuilder cart = new StringBuilder();
+            cart.Append("Cart Items:");
+            foreach (var item in ShoppingCart)
+            {
+                cart.AppendLine("\t" + item.GetName());
+            }
+            return cart.ToString();
+        }
+    }
+}
